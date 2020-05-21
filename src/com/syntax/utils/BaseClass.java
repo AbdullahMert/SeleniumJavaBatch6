95a87de107e7cdbf44b93832e4ce32be2cb2a304
AbdullahMert <abdbirisi77@gmail.com>
2020-05-21 06:05:17 -0500

    May 21

--- src/com/syntax/utils/BaseClass.java
+++ src/com/syntax/utils/BaseClass.java
@@ -4,31 +4,33 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
+
 	public static WebDriver driver;
 
 	public static WebDriver setUp() {
-		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
 
+		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+		System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "true");
+		
 		switch (ConfigsReader.getProperty("browser").toLowerCase()) {
 
 		case "chrome":
 			System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH);
 			driver = new ChromeDriver();
 			break;
-
 		case "firefox":
 			System.setProperty("webdriver.gecko.driver", Constants.GECKO_DRIVER_PATH);
-			driver = new ChromeDriver();
+			driver = new FirefoxDriver();
 			break;
-
 		default:
 			throw new RuntimeException("Browser is not supported");
-
 		}
 
-		driver.manage().window().maximize();
+		//driver.manage().window().fullscreen();
 		driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
 		driver.get(ConfigsReader.getProperty("url"));
 		
@@ -40,5 +42,4 @@ public class BaseClass {
 			driver.quit();
 		}
 	}
-
 }
--- src/com/syntax/utils/CommonMethods.java
+++ src/com/syntax/utils/CommonMethods.java
@@ -1,8 +1,10 @@
 package com.syntax.utils;
 
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchFrameException;
 import org.openqa.selenium.WebElement;
@@ -14,7 +16,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class CommonMethods extends BaseClass {
 
 	/**
-	 * Method that clears and send keys
+	 * Method that clears and sends keys
 	 * 
 	 * @param element
 	 * @param text
@@ -25,40 +27,40 @@ public class CommonMethods extends BaseClass {
 	}
 
 	/**
-	 * method checks if radio/checkbox is enabled and clicks it
+	 * Method checks if radio/checkbox is enabled and clicks it
 	 * 
-	 * @param radioOrCheckbox
+	 * @param radioOrcheckbox
 	 * @param value
 	 */
-	public static void clickRadioOrCheckbox(List<WebElement> radioOrCheckbox, String value) {
+	public static void clickRadioOrCheckbox(List<WebElement> radioOrcheckbox, String value) {
+
 		String actualValue;
-		for (WebElement el : radioOrCheckbox) {
+
+		for (WebElement el : radioOrcheckbox) {
 			actualValue = el.getAttribute("value").trim();
 			if (el.isEnabled() && actualValue.equals(value)) {
 				el.click();
 				break;
 			}
-
 		}
 	}
 
 	/**
-	 * Method that checks if text is there and then selects it -works only with DD
-	 * with select attribute
+	 * Method that checks if text is there and then selects it
 	 * 
 	 * @param element
 	 * @param textToSelect
 	 */
 	public static void selectDdValue(WebElement element, String textToSelect) {
+
 		try {
 			Select select = new Select(element);
+
 			List<WebElement> options = select.getOptions();
+
 			for (WebElement el : options) {
-				String value = el.getText();
-				if (value.equals(textToSelect)) {
-					// select.selectByVisibleText(textToSelect); this one or the next line works the
-					// same
-					el.click();
+				if (el.getText().equals(textToSelect)) {
+					select.selectByVisibleText(textToSelect);
 					break;
 				}
 			}
@@ -66,21 +68,21 @@ public class CommonMethods extends BaseClass {
 		} catch (UnexpectedTagNameException e) {
 			e.printStackTrace();
 		}
-
 	}
 
-	/***
-	 * Method that checks if index is valid and then selects by index value - works
-	 * only with DD with select tag
+	/**
+	 * Method that selects value by index
 	 * 
 	 * @param element
 	 * @param index
 	 */
 	public static void selectDdValue(WebElement element, int index) {
+
 		try {
 			Select select = new Select(element);
-			int indSize = select.getOptions().size();
-			if (index < indSize) {
+			int size = select.getOptions().size();
+
+			if (size > index) {
 				select.selectByIndex(index);
 			}
 		} catch (UnexpectedTagNameException e) {
@@ -88,42 +90,69 @@ public class CommonMethods extends BaseClass {
 		}
 	}
 
-	// alerts==============================================================================
+	/**
+	 * Methods that accept alerts and catches exception if alert is not present
+	 */
 	public static void acceptAlert() {
+
 		try {
 			Alert alert = driver.switchTo().alert();
 			alert.accept();
+
 		} catch (NoAlertPresentException e) {
 			e.printStackTrace();
 		}
-
 	}
 
+	/**
+	 * Methods that dismiss alerts and catches exception if alert is not present
+	 */
 	public static void dismissAlert() {
+
 		try {
 			Alert alert = driver.switchTo().alert();
 			alert.dismiss();
-		} catch (NoAlertPresentException e) {
-
-		}
-	}
-
-	public static String getAlertText() {
-		String alertText = null;
-		try {
-
-			Alert alert = driver.switchTo().alert();
-			alertText = alert.getText();
 
 		} catch (NoAlertPresentException e) {
 			e.printStackTrace();
-
 		}
+	}
+
+	/**
+	 * Methods that gets text of alert and catches exception if alert is not present
+	 * 
+	 * @return String alert text
+	 */
+	public static String getAlertText() {
+
+		String alertText = null;
+
+		try {
+			Alert alert = driver.switchTo().alert();
+			alertText = alert.getText();
+		} catch (NoAlertPresentException e) {
+			e.printStackTrace();
+		}
+
 		return alertText;
 	}
 
-// frames===================================================================
+	/**
+	 * Methods that sends text to alert and catches exception if alert is not
+	 * present
+	 * 
+	 */
+	public static void sendAlertText(String text) {
+		try {
+			Alert alert = driver.switchTo().alert();
+			alert.sendKeys(text);
+		} catch (NoAlertPresentException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void switchToFrame(String nameOrId) {
+
 		try {
 			driver.switchTo().frame(nameOrId);
 		} catch (NoSuchFrameException e) {
@@ -132,14 +161,36 @@ public class CommonMethods extends BaseClass {
 	}
 
 	public static void switchToFrame(WebElement element) {
+
 		try {
 			driver.switchTo().frame(element);
 		} catch (NoSuchFrameException e) {
 			e.printStackTrace();
 		}
-
 	}
-	// page waits ================================================================
+
+	public static void switchToFrame(int index) {
+
+		try {
+			driver.switchTo().frame(index);
+		} catch (NoSuchFrameException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Method switches focus to child window
+	 */
+	public static void switchToChildWindow() {
+		String mainWindow = driver.getWindowHandle();
+		Set<String> windows = driver.getWindowHandles();
+		for (String window : windows) {
+			if (!window.equals(mainWindow)) {
+				driver.switchTo().window(window);
+				break;
+			}
+		}
+	}
 
 	public static WebDriverWait getWaitObject() {
 		WebDriverWait wait = new WebDriverWait(driver, Constants.EXPLICIT_WAIT_TIME);
@@ -147,12 +198,55 @@ public class CommonMethods extends BaseClass {
 	}
 
 	public static WebElement waitForClickability(WebElement element) {
-		getWaitObject().until(ExpectedConditions.elementToBeClickable(element));
-		return element;
+		return getWaitObject().until(ExpectedConditions.elementToBeClickable(element));
+	}
+
+	public static WebElement waitForVisibility(WebElement element) {
+		return getWaitObject().until(ExpectedConditions.visibilityOf(element));
 	}
 
 	public static void click(WebElement element) {
 		waitForClickability(element);
 		element.click();
+	}
+
+	public static JavascriptExecutor getJSObject() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		return js;
+	}
+
+	public static void jsClick(WebElement element) {
+		getJSObject().executeScript("arguments[0].click();", element);
+	}
+
+	public static void scrollToElement(WebElement element) {
+		getJSObject().executeScript("arguments[0].scrollIntoView(true);", element);
+	}
+
+	/**
+	 * Method that will scroll the page down based on the passed pixel parameters
+	 * 
+	 * @param pixel
+	 */
+	public static void scrollDown(int pixel) {
+		getJSObject().executeScript("window.scrollBy(0," + pixel + ")");
+	}
+
+	/**
+	 * Method that will scroll the page up based on the passed pixel parameters
+	 * 
+	 * @param pixel
+	 */
+	public static void scrollUp(int pixel) {
+		getJSObject().executeScript("window.scrollBy(0,-" + pixel + ")");
+	}
+
+	
+	public static void wait(int second) {
+		try {
+			Thread.sleep(second * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
